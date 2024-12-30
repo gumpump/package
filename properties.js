3b69501c976aa31823629c77aaac974857b8f825
gumpump <richguy913@gmail.com>
2024-12-30 13:42:14 +0100

    Added property view for faces. Added ability to clear the property view

--- properties.js
+++ properties.js
@@ -4,14 +4,28 @@ import { Face } from "./face.js"
 
 export class Properties
 {
+	static clear ()
+	{
+		console.log ("Property view cleared");
+
+		const hTarget = document.getElementById ("sidebar-object_properties");
+
+		if (hTarget == null)
+		{
+			console.log ("Could not find target");
+
+			return;
+		}
+
+		hTarget.innerHTML = "";
+	}
+
 	//////////////
 	// BUILDERS //
 	//////////////
 
 	static buildPointView (object)
 	{
-		console.log ("Property view built");
-
 		if (object == null)
 		{
 			console.log ("Invalid object");
@@ -39,8 +53,42 @@ export class Properties
 		propHTML += '<p>Type: <span id="sidebar-object_properties_type">' + "Point" + '</span></p>';
 		propHTML += '<p>ID: <span id="sidebar-object_properties_id">' + object.getId () + '</span></p>';
 
-		console.log (propHTML);
+		hTarget.innerHTML = propHTML;
+
+		console.log ("Property view built");
+	}
+
+	static buildFaceView (object)
+	{
+		if (object == null)
+		{
+			console.log ("Invalid object");
+
+			return;
+		}
+
+		if ((object instanceof Face) == false)
+		{
+			console.log ("Object is not a Face");
+
+			return;
+		}
+
+		const hTarget = document.getElementById ("sidebar-object_properties");
+
+		if (hTarget == null)
+		{
+			console.log ("Could not find target");
+
+			return;
+		}
+
+		var propHTML = "";
+		propHTML += '<p>Type: <span id="sidebar-object_properties_type">' + "Face" + '</span></p>';
+		propHTML += '<p>ID: <span id="sidebar-object_properties_id">' + object.getId () + '</span></p>';
 
 		hTarget.innerHTML = propHTML;
+
+		console.log ("Property view built");
 	}
 }
